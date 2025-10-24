fe7135106a00e54e67c871c10b6ce6dcd86ba5ef
Rassiatou <assyacoulibaly225@gmail.com>
2025-10-24 11:35:14 -0400

    modification

--- src/routes/avis.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { list, create, remove } from "../controllers/avis.controller.js";
-const router = Router();
-
-router.get("/", list);
-router.post("/", create); // tu peux protéger plus tard avec JWT si tu veux
-router.delete("/:idAvis", remove);
-
-export default router;
